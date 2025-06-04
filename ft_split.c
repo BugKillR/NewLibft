85c05a1495538413d99d598e9f3e4026b66ebaf2
BugKillR <kayrabora.keskin@gmail.com>
2025-06-04 20:47:01 +0300

    Commit 4

--- ft_split.c
+++ ft_split.c
@@ -6,93 +6,81 @@
 /*   By: kkeskin <kkeskin@student.42istanbul.com.tr +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 18:51:57 by kkeskin           #+#    #+#             */
-/*   Updated: 2025/06/03 08:22:21 by kkeskin          ###   ########.fr       */
+/*   Updated: 2025/06/04 20:43:10 by kkeskin          ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static char    *ft_mallocandcpy(char const *s, char c, size_t lastInit, size_t i)
+static char	*ft_mallocandcpy(char const *s, char c, size_t lastInit, size_t i)
 {
-    char    *arr;
-    size_t        k;
+	char	*arr;
+	size_t	k;
 
-    k = 0;
-    arr = (char *)malloc((i - lastInit) + 1);
-    if (!arr)
-        return (NULL);
-    while (lastInit < i)
-    {
-        arr[k] = s[lastInit];
-        k++;
-        lastInit++;
-    }
-    arr[k] = '\0';
-    return (arr);
+	k = 0;
+	arr = (char *)malloc((i - lastInit) + 1);
+	if (!arr)
+		return (NULL);
+	while (lastInit < i)
+	{
+		arr[k] = s[lastInit];
+		k++;
+		lastInit++;
+	}
+	arr[k] = '\0';
+	return (arr);
 }
 
-static void    ft_splitwords(char const *s, char c, char **split)
+static void	ft_splitwords(char const *s, char c, char **split)
 {
-    size_t	i;
-    size_t	splitIndex;
-    size_t	lastInit;
-	size_t	isInSeparator;
+	size_t	i;
+	size_t	split_index;
+	size_t	last_init;
 
-    i = 0;
-    lastInit = 0;
-    splitIndex = 0;
-	isInSeparator = 0;
-    while (s[i])
-    {
-        if (s[i] == c)
-        {
-            split[splitIndex] = ft_mallocandcpy(s, c, lastInit, i);
-            splitIndex++;
-            lastInit = i + 1;
-        }
-        i++;
-    }
-	split[splitIndex++] = ft_mallocandcpy(s, c, lastInit, i);
-	split[splitIndex] = NULL;
+	i = 0;
+	last_init = 0;
+	split_index = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			split[split_index] = ft_mallocandcpy(s, c, last_init, i);
+			split_index++;
+			last_init = i + 1;
+		}
+		i++;
+	}
+	split[split_index++] = ft_mallocandcpy(s, c, last_init, i);
+	split[split_index] = NULL;
 }
 
-static int    ft_wordcount(char const *s, char c)
+static int	ft_wordcount(char const *s, char c)
 {
-    size_t    i;
-    size_t    count;
+	size_t	i;
+	size_t	count;
 
-    if (s[0] == "\0")
-        return (0);
-    i = 0;
-    count = 1;
-    while (s[i])
-    {
-        if (s[i] == c)
-            count++;
-        i++;
-    }
-    return (count);
+	if (s[0] == '\0')
+		return (0);
+	i = 0;
+	count = 1;
+	while (s[i])
+	{
+		if (s[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
 }
 
-char    **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    char    **split;
+	char	**split;
 
-    if (!s)
-        return (NULL);
-    split = (char **)malloc((ft_wordcount(s, c) + 1) * sizeof(char *));
-    if (!split)
-        return (NULL);
-    ft_splitwords(s, c, split);
-    return (split);
-}
-
-int    main(void)
-{
-    char    **arr;
-
-    arr = ft_split("Hello cruel world!", ' ');
-    printf("%s\n%s\n%s\n", arr[0], arr[1], arr[2]);
-    return (0);
+	if (!s)
+		return (NULL);
+	split = (char **)malloc((ft_wordcount(s, c) + 1) * sizeof(char *));
+	if (!split)
+		return (NULL);
+	ft_splitwords(s, c, split);
+	return (split);
 }
